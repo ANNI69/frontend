ee6bde31ed46c2853e34fcb78e00145f3986702e
Aniruddh Nagare <84516362+ANNI69@users.noreply.github.com>
2024-07-26 15:23:25 +0530

    resizeable done

--- unishare/src/app/page.tsx
+++ unishare/src/app/page.tsx
@@ -8,32 +8,45 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="max-w-md rounded-lg border"
-      >
-        <ResizablePanel defaultSize={50}>
-          <div className="flex h-[200px] items-center justify-center p-6">
-            <span className="font-semibold">One</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={50}>
-          <ResizablePanelGroup direction="vertical">
-            <ResizablePanel defaultSize={25}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Two</span>
-              </div>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel defaultSize={75}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Three</span>
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <div className="flex flex-col h-screen w-screen">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="max-w-screen rounded-lg border"
+        >
+          <ResizableHandle />
+          <ResizablePanel defaultSize={50}>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel defaultSize={10}>
+                <div className="flex h-full items-center justify-center p-6">
+                  <span className="font-semibold">Two</span>
+                </div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={90}>
+                <ResizablePanelGroup direction="horizontal">
+                  <ResizablePanel defaultSize={25}>
+                    <div className="flex h-full items-center justify-center p-6">
+                      <span className="font-semibold">Three</span>
+                    </div>
+                  </ResizablePanel>
+                  <ResizableHandle />
+                  <ResizablePanel defaultSize={90}>
+                    <div className="flex h-full items-center justify-center p-6">
+                      <span className="font-semibold">Four</span>
+                    </div>
+                  </ResizablePanel>
+                  <ResizableHandle />
+                  <ResizablePanel defaultSize={30}>
+                    <div className="flex h-full items-center justify-center p-6">
+                      <span className="font-semibold">Four</span>
+                    </div>
+                  </ResizablePanel>
+                </ResizablePanelGroup>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </>
   );
 }
